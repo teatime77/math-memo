c360dbbee04bd05568a0922bed2e8f22a486301e
teatime77 <teatime77@live.jp>
2019-05-01 14:12:40 +0900

    エッジのラベル表示の途中

--- ts/main.ts
+++ ts/main.ts
@@ -215,27 +215,6 @@ function stringify_doc(doc: Doc){
     }
 }
 
-function get_indent(line: string) : [number, string]{
-    var indent = 0;
-    while(true){
-        if(line.startsWith("\t")){
-            indent++;
-            line = line.substring(1);    
-        }
-        else if(line.startsWith("    ")){
-            indent++;
-            line = line.substring(4);
-        }
-        else{
-            return [indent, line];
-        }
-    }
-}
-
-function tab(indent: number){
-    return " ".repeat(4 * indent);
-}
-
 export class OrderedMap<K, V> {
     map: any;
     keys: K[];
@@ -307,87 +286,16 @@ export class TextBlock {
         return this.inputs.map(x => x.src_id);
     }
 
-    make_html_lines(){
-        var html_lines = [];            
-
-        var in_math = false;
-        var ul_indent = -1;
-        var prev_line = "";
-        for(let current_line of this.lines){
-            var current_line_trim = current_line.trim();
-
-            let [indent, line] = get_indent(current_line);
-            indent--;
-
-            if(current_line_trim == "$$"){
-                in_math = ! in_math;
-                html_lines.push(current_line);
-            }
-            else{
-                if(in_math){
-
-                    html_lines.push(current_line);
-                }
-                else{
-
-                    if(line.startsWith("# ")){
-                        html_lines.push(tab(indent + 1) + "<strong><span>" + line.substring(2) + "</span></strong><br/>")
-                    }
-                    else if(line.startsWith("- ")){
-                        if(ul_indent < indent){
-                            console.assert(ul_indent + 1 == indent);
-                            html_lines.push(tab(indent) + "<ul>")
-                            ul_indent++;
-                        }
-                        else{
-                            while(ul_indent > indent){
-                                html_lines.push(tab(ul_indent) + "</ul>")
-                                ul_indent--;
-                            }                            
-                        }
-                        html_lines.push(tab(indent + 1) + "<li><span>" + line.substring(2) + "</span></li>")
-                    }
-                    else{
-
-                        if(prev_line.endsWith("</li>")){
-                            html_lines[html_lines.length - 1] = prev_line.substring(0, prev_line.length - 5) + "<br/>";
-                            html_lines.push(tab(indent + 1) + "<span>" + line + "</span></li>")
-                        }
-                        else{
-
-                            html_lines.push(tab(indent + 1) + "<span>" + line + "</span><br/>")
-                        }
-                    }
-                }
-            }
-
-            prev_line = html_lines[html_lines.length - 1];
-        }
-
-        while(ul_indent != -1){
-            html_lines.push(tab(ul_indent) + "</ul>")
-            ul_indent--;
-        }
-
-        return html_lines;
-    }
 
     /*
         HTML要素を作る。
     */
     make(){
-        var html_lines = this.make_html_lines();
-
         this.ele = document.createElement("div");
-        this.ele.innerHTML = html_lines.join("\n");
-        this.ele.id = this.id;
+        this.ele.innerHTML = make_html_lines(this.lines);
         document.body.appendChild(this.ele);
         
-        var br = document.createElement("br");
-        document.body.appendChild(br);
-
         dom_list.push(this.ele);
-        dom_list.push(br);
     }
 }
 
